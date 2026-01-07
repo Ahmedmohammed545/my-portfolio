2cb9041203ca3b61d35ed405767d84b9f2d0c2d0
Ahmedmohammed545 <139759327+Ahmedmohammed545@users.noreply.github.com>
2026-01-07 15:17:17 +0300

    Update siteConfig.ts

--- lib/siteConfig.ts
+++ lib/siteConfig.ts
@@ -1,7 +1,7 @@
-export const CONTACT_EMAIL = "ahmedmohammedaddisababa@gmail.com"
+dexport const CONTACT_EMAIL = "ahmedmohammedaddisababa@gmail.com"
 
 export const siteMetadata = {
-  name: "Ahmed Mohammed",
+  name: "Ahmed Mohammedseid",
   tagline: "Building technology for positive impact",
   shortBio:
     "High school student passionate about leveraging technology to create meaningful change in my community and beyond.",
